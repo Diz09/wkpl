a5433d2975d095ea4d2d2843e01ef427f7bbf2c5
Dimas <izzulhaqzaindimad@gmail.com>
2023-10-19 15:14:43 +0700

    first

--- SimpleTest.php
+++ SimpleTest.php
@@ -1,22 +1,30 @@
 <?php
-
+//mengimport 1 libariry PHPUnit\Framework\TestCase yang ada pada composer
 use PHPUnit\Framework\TestCase;
 
-//class yang di tes
+//memuat/memanggil file dari wordcount.php
 require_once "wordcount.php";
 
 //class untuk melakukan testing
+//simpltest merupakan turunan/extends dari testcase
 class SimpleTest extends TestCase {
 
     public function test() {
-        //gunakan class yang ingin di tes
+        //membuat objek baru yang mengimplementasikan wordcount
         $wc = new wordcount();
-
-        //masukkan parameter 4 kata
+        
+        //pertanyaan
+        //masukkan parameter 4 kata (true) atau lebih (false), berdasarkan nanti pada $this
         $TestSentence = "aku orang kaya dan genius";
+        //membuat objek baru ($Wordcount)
+        //didalamnya dijalankan fungsi countword yang dipanggil dari objek $wc yang berfungsi untuk melihat jumlah kata dan menghitungnya
         $Wordcount = $wc -> countwords($TestSentence);
 
+        //jawaban
         //pesan jika gagal
-        $this -> assertEquals(4, $Wordcount);
+        //$this berupa penunjuk untuk objek yang sedang memakai method
+        //assertEquals digunakan untuk membandingkan nilai yang diharapkan(5 kata) dengan nilai yang ada(wordcount)
+        $this -> assertEquals(5, $Wordcount);
     }
 }
+?>
--- wordcount.php
+++ wordcount.php
@@ -1,7 +1,19 @@
 <?php
 //File : wordcount.php
 class wordcount {
+    //fungsi untuk menghitung kata(spasi)
     public function countwords($sentence) {
         return count(explode(" ", $sentence));
     }
 }
+
+/*
+explode berfugsi untuk memecah data string dan mengubahnya menjadi data array berdasarkan delimiter/karakter(spasi)
+count berfungsi untuk menghitung array dari string telah dipecah
+$sentece merupakan variable yang berisi kalimat
+
+jadi aplikasi ini akan menangkap data string/kalimat lalu memecahnya berdasarkan delimiter(spasi) dan memurahnya menjadi array 
+    lalu akan dihitung banyak dari array data akan dikembalikan kepada countwords yang akan dipakai pada SimpleTest
+*/
+
+?>
